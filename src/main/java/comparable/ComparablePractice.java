919a99498d0db759322843e0d512697b48a04e46
Cumbassador <163416241+Cumbassador@users.noreply.github.com>
2024-03-23 01:07:04 +0300

    version0.0.3

--- src/main/java/comparable/ComparablePractice.java
+++ src/main/java/comparable/ComparablePractice.java
@@ -13,6 +13,16 @@ public class ComparablePractice {
         System.out.println("Arrays.toString(boys) = " + Arrays.toString(boys));
         Arrays.sort(boys);
         System.out.println("Arrays.toString(boys) = " + Arrays.toString(boys));
+        Person anatoliy = new Person("Anatoliy","Zhukov",25);
+        Person vladimir = new Person("Vladimir","Semenov",30);
+        Person anatoli = new Person("Anatoliy","Zhukov",19);
+        Person andrey= new Person("Andrey","Semenov",30);
+        Person[] pers = new Person[] {anatoliy,anatoli,vladimir,andrey};
+        System.out.println("Arrays.toString(pers) = " + Arrays.toString(pers));
+        Arrays.sort(pers);
+        System.out.println("Arrays.toString(pers) = " + Arrays.toString(pers));
+        Person andrey2 = andrey.clone();
+        System.out.println("andrey2 = " + andrey2);
 
 
     }
--- src/main/java/comparable/Person.java
+++ src/main/java/comparable/Person.java
@@ -1,19 +1,50 @@
 package comparable;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@AllArgsConstructor
 @Data
-public class Person implements Comparable<Person> {
+@AllArgsConstructor
+public class Person implements Comparable<Person>, Cloneable {
     private final String name;
     private final String secondName;
     private final int age;
-
+//    int i = Integer.parseInt(secondName) - Integer.parseInt(o.secondName);
+//        if (i == 0) {
+//        i = Integer.parseInt(name) - Integer.parseInt(o.name);
+//        if (i == 0) {
+//            i = age - o.age;
+//
+//        }
+//    }
+//
+//        return i;
 
     @Override
     public int compareTo(Person o) {
+        int i =secondName.hashCode()-o.secondName.hashCode();
+        if (i==0){
+            i =name.hashCode()-o.name.hashCode();{
+                if(i == 0 ){
+                    i = age - o.age;
+                }
+            }
+        }
+        return i;
+    }
 
-        return ;
+    protected boolean canEqual(final Object other) {
+        return other instanceof Person;
+    }
+
+
+    @Override
+    public Person clone() {
+        try {
+            Person clone = (Person) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 }
